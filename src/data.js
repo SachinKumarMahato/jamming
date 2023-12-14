2d5d1af2f704dc7a4273e049e3f6a13cce746bc7
Sachin <sachin333777@outlook.com>
2023-12-14 10:29:45 +0530

    reset

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const data = [
+  {
+    id: 1,
+    name: 'Harley in Hawaii',
+    artist: 'katy parry',
+    album: 'Harley in Hawaii'
+  },
+  {
+    id: 2,
+    name: 'enemy',
+    artist: 'Tommy',
+    album: 'cinematic song'
+  }
+]
+
+export default data
